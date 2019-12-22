193c6b3a2c68ecdcce058861b9ecbab7dd1fd4ce
Maria <mariabrock414@gmail.com>
2019-12-21 21:11:43 -0600

    added event listeners

---
+++ src/javascripts/components/projectCards/projectCards.js
@@ -0,0 +1,25 @@
+import './projectCards.scss';
+import projectData from '../../helpers/data/projectsData';
+import utilities from '../../helpers/utilities';
+
+const displayProjectCards = () => {
+  projectData.getProjectData()
+    .then((projects) => {
+      let domString = '';
+      projects.forEach((project) => {
+        domString += `
+        <div id="card">
+            <h2 id="title">${project.title}</h2>
+            <img src="${project.screenshot}" alt="Image of ${project.title}" />
+            <p>${project.description}</p>
+            <p>${project.technologiesUsed}</p>
+            <p id="linkToProject">${project.url}</p>
+        </div>
+        `;
+      });
+      utilities.printToDom('projectsPage', domString);
+    })
+    .catch((error) => console.error(error));
+};
+
+export default { displayProjectCards };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,1 +1,16 @@
 import '../styles/main.scss';
+
+import firebase from 'firebase';
+import 'bootstrap';
+
+import apiKeys from './helpers/apiKeys.json';
+import projectCards from './components/projectCards/projectCards';
+
+console.error('this will be my site');
+
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  projectCards.createProjectCards();
+};
+
+init();
